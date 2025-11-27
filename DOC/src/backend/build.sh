c950d6aed42ed68a0b4ef1803501846ba87f714f
Victor <victorpachecogontijo@gmail.com>
2025-11-26 21:57:06 -0300

    superuser

--- DOC/src/backend/build.sh
+++ DOC/src/backend/build.sh
@@ -5,6 +5,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser
 
 # Opcional: Rodar o script de setup inicial automaticamente no deploy
 # python setup_initial.py
